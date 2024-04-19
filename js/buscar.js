02fdd193fac54dc4a90c4a4866d154d781db334a
NavarroT <tiago,navarro@sptech.school>
2024-04-18 21:16:46 -0300

    aula 18-04

--- js/buscar.js
+++ js/buscar.js
@@ -1,18 +1,42 @@
 async function buscar(){
-    console.log("Passei por aqui")
-
     const resposta = await fetch("https://660f44ec356b87a55c511089.mockapi.io/agendas"); // apenas trocar pelo "localhost:....."
     const respostaDadosAgenda = await resposta.json(); 
 
     console.log("Resposta: ", respostaDadosAgenda);
 
-    const cards = document.getElementById("card_games");
+    const cards = document.getElementById("cards_games");
 
     cards.innerHTML = respostaDadosAgenda.map((itemAgenda) => {
-            return "TESTE"
-    });
+            return `
+            <div class="cardItem">
+                <div class="dataGame"> 
+                    <img src="../imagens/calendar-solid.svg" alt=""> ${itemAgenda.dataJogo} 
+                </div>
+                <img class="img-background" src="${itemAgenda.urlImagem}" alt="">
+                <div class="descricao">
+                    <p> <strong>${itemAgenda.nome}</strong> </p>
+                    <p> Descrição de como vai funcionar o GamePlay, frase legal para chamar a galera :D </p>
+                    <p><strong>Gamers: </strong></p>
+
+                    <div class="gamers">
+                    ${
+                        itemAgenda.gamers.map((gamerItem) => {
+                            return `
+                            <div class="gamerItem">${gamerItem}</div>
+                            `
+                        }).join('')
+                    }
+                    </div>
+
+                  
+                </div> 
+                <div class="assistir">
+                        <a class="assistirItem"> <img src="../imagens/youtube.svg" alt=""> Assistir </a>
+                </div>
+            </div>
+            `
+    }).join('');
 };
 
-console.log("antes de buscar")
 buscar();
-console.log("depois do buscar")
+
